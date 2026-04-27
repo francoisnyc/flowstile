1902d19c0160a828a733d7c6ee068519b7282603
francoisnyc <fchevresson@gmail.com>
2026-04-26 21:46:32 -0400

    feat: add entity barrel export and fix strictPropertyInitialization for TypeORM

---
+++ packages/server/src/entities/index.ts
@@ -0,0 +1,7 @@
+export { User } from './user.entity.js';
+export { Group } from './group.entity.js';
+export { Role } from './role.entity.js';
+export { FormDefinition } from './form-definition.entity.js';
+export { ProcessDefinition } from './process-definition.entity.js';
+export { TaskDefinition } from './task-definition.entity.js';
+export { Task } from './task.entity.js';
